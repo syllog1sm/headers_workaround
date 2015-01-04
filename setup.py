5caf6d2b9ab666b94eb1b03bb9af7e75c510685d
Matthew Honnibal <honnibal@gmail.com>
2015-01-04 21:54:28 +1100

    * Increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name="headers_workaround",
     packages=["headers_workaround"],
-    version="0.14",
+    version="0.15",
     author="Matthew Honnibal",
     author_email="honnibal@gmail.com",
     url="http://github.com/syllog1sm/headers_workaround",
